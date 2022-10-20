517262430a0314f45e26512e44fb38ab2fa1d01b
Rizki Rendianto <rendiyanto2016@gmail.com>
2022-10-20 13:42:05 +0700

    Menambah Logo Pada Title

--- application/views/psp/coba.php
+++ application/views/psp/coba.php
@@ -20,6 +20,7 @@
             <!-- Navigation-->
             <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
                 <div class="container px-5">
+                    <i class="feature mb-3"> <img src="assets/img/LOGO_SMP.png" height="30" width="30" margin=""> </i>
                     <a class="navbar-brand" href="index.html">SMPN 1 GROBOGAN</a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
