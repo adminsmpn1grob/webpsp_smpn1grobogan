94532c457d00d29805460c0e7870ec6f68989575
Rizki Rendianto <rendiyanto2016@gmail.com>
2022-10-20 13:29:56 +0700

    Menambah Fav-Icon + Theme

--- application/views/psp/coba.php
+++ application/views/psp/coba.php
@@ -7,7 +7,7 @@
         <meta name="author" content="" />
         <title>Website PSP SMP N 1 Grobogan</title>
         <!-- Favicon-->
-        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <link rel="icon" type="image/x-icon" href="assets/img/LOGO_SMP.png" />
         <!-- Bootstrap icons-->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
         <!-- Core theme CSS (includes Bootstrap)-->
