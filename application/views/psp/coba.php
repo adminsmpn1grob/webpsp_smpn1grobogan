b90becf3364501e52621aa2f15c8a074ad6e0787
Rizki Rendianto <rendiyanto2016@gmail.com>
2022-10-20 14:02:21 +0700

    Mengubah Img Title

--- application/views/psp/coba.php
+++ application/views/psp/coba.php
@@ -13,6 +13,7 @@
         <!-- Core theme CSS (includes Bootstrap)-->
         <link href="css/styles.css" rel="stylesheet" />
         <link href="<?php echo base_url('assets/css/styles.css');?>" rel="stylesheet">
+        <link href="<?php echo base_url('assets/css/mystyles.css');?>" rel="stylesheet">
         <link href="<?php echo base_url('assets/js/Script.js');?>" rel="stylesheet">
     </head>
     <body class="d-flex flex-column h-100">
@@ -20,8 +21,12 @@
             <!-- Navigation-->
             <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
                 <div class="container px-5">
-                    <i class="feature mb-3"> <img src="assets/img/LOGO_SMP.png" height="30" width="30" margin=""> </i>
-                    <a class="navbar-brand" href="index.html">SMPN 1 GROBOGAN</a>
+                    <div class="container">
+                        <div class="container">
+                            <i class="img-title"> <img src="assets/img/LOGO_SMP.png" height="30" width="30" margin-left="40"> </i>
+                        </div>
+                        <a class="navbar-brand" href="#">SMPN 1 GROBOGAN</a>
+                    </div>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
